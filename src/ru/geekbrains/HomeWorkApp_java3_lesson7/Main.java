4aa73c7afcfbbca5279db119d5cffcafd4aa2359
Mercesletifer47 <art-matv98@yandex.ru>
2021-11-01 13:18:13 +0300

    dev actual

--- src/ru/geekbrains/HomeWorkApp_java3_lesson7/Main.java
+++ src/ru/geekbrains/HomeWorkApp_java3_lesson7/Main.java
@@ -3,6 +3,6 @@ package ru.geekbrains.HomeWorkApp_java3_lesson7;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        StartTest.start(MyTest.class);
     }
 }
